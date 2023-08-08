697c96abe6376ce3f5291bb072bc2842a320ec16
SergeiKazanin <serega7667@gmail.com>
2023-08-08 19:26:18 +0700

    added partners

--- src/components/Whoosh/Partners/index.js
+++ src/components/Whoosh/Partners/index.js
@@ -1,7 +1,54 @@
 import style from "./index.module.scss";
-
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 import Icons from "./sprite.svg";
+import "@splidejs/react-splide/css";
+
+import { ReactComponent as Alpfa } from "./images/alfa.svg";
+import { ReactComponent as Fagor } from "./images/fagor.svg";
+import { ReactComponent as GuitaCenter } from "./images/guitaCenter.svg";
+import { ReactComponent as RadioShack } from "./images/RadioShack.svg";
+import { ReactComponent as Tendo } from "./images/tendo.svg";
 
 export const Partners = () => {
-  return <div className={style.partners}></div>;
+  return (
+    <div className={style.partners}>
+      <div className={style.partners__headerBlock}>
+        <span className={style.partners__headerText}>Our Partners</span>
+        <div className={style.partners__lelColab}>
+          <svg className={style.partners__briefcaseSvg}>
+            <use xlinkHref={Icons + "#briefcase"}></use>
+          </svg>
+          <span className={style.partners__colabText}>Let's collaborate</span>
+        </div>
+      </div>
+      <div className={style.partners__splide}>
+        <Splide
+          options={{
+            width: 1000,
+            gap: "4px",
+            type: "loop",
+            perPage: 3,
+            focus: "center",
+          }}
+          aria-label="Partners"
+        >
+          <SplideSlide>
+            <Alpfa className={style.partners__svg} />
+          </SplideSlide>
+          <SplideSlide>
+            <Fagor className={style.partners__svg} />
+          </SplideSlide>
+          <SplideSlide>
+            <GuitaCenter className={style.partners__svg} />
+          </SplideSlide>
+          <SplideSlide>
+            <RadioShack className={style.partners__svg} />
+          </SplideSlide>
+          <SplideSlide>
+            <Tendo className={style.partners__svg} />
+          </SplideSlide>
+        </Splide>
+      </div>
+    </div>
+  );
 };

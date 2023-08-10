69bd21f78212ed8376a4e7ba9da7ba3be8acb424
SergeiKazanin <serega7667@gmail.com>
2023-08-10 11:24:50 +0700

    added slider color

--- src/components/Whoosh/Partners/index.js
+++ src/components/Whoosh/Partners/index.js
@@ -2,12 +2,14 @@ import style from "./index.module.scss";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import Icons from "./sprite.svg";
 import "@splidejs/react-splide/css";
+import cl from "classnames";
 
 import { ReactComponent as Alpfa } from "./images/alfa.svg";
 import { ReactComponent as Fagor } from "./images/fagor.svg";
 import { ReactComponent as GuitaCenter } from "./images/guitaCenter.svg";
 import { ReactComponent as RadioShack } from "./images/RadioShack.svg";
 import { ReactComponent as Tendo } from "./images/tendo.svg";
+import { useEffect } from "react";
 
 export const Partners = () => {
   return (
@@ -27,6 +29,7 @@ export const Partners = () => {
             width: 1104,
             type: "loop",
             perPage: 5,
+            perMove: 1,
             focus: "center",
             pagination: false,
             arrows: false,
@@ -36,30 +39,55 @@ export const Partners = () => {
             wheelSleep: 50,
           }}
           aria-label="Partners"
+          onInactive={() => {
+            const slide1 = document.querySelectorAll(".splide__slide");
+            for (let i = 0; i < slide1.length; i++) {
+              const svgSearch = slide1[i].querySelector(".svg-search");
+              if (svgSearch) {
+                svgSearch.classList.add("splide__svg");
+              }
+            }
+            const slide = document.querySelectorAll(
+              ".splide__slide.is-visible.is-active"
+            );
+            for (let i = 0; i < slide.length; i++) {
+              const svgSearch = slide[i].querySelector(".svg-search");
+              svgSearch?.classList.remove("splide__svg");
+            }
+          }}
+          onReady={() => {
+            const slide = document.querySelectorAll(
+              ".splide__slide.is-visible.is-active"
+            );
+            for (let i = 0; i < slide.length; i++) {
+              const svgSearch = slide[i].querySelector(".svg-search");
+              svgSearch?.classList.remove("splide__svg");
+            }
+          }}
         >
           <SplideSlide>
-            <div className={style.partners__slpideElem}>
-              <Alpfa className={style.partners__svg} />
+            <div className={cl(style.partners__slpideElem, "slide-search")}>
+              <Alpfa className={cl("svg-search", "splide__svg")} />
             </div>
           </SplideSlide>
           <SplideSlide>
-            <div className={style.partners__slpideElem}>
-              <Fagor className={style.partners__svg} />
+            <div className={cl(style.partners__slpideElem, "slide-search")}>
+              <Fagor className={cl("svg-search", "splide__svg")} />
             </div>
           </SplideSlide>
           <SplideSlide>
-            <div className={style.partners__slpideElem}>
-              <GuitaCenter className={style.partners__svg} />
+            <div className={cl(style.partners__slpideElem, "slide-search")}>
+              <GuitaCenter className={cl("svg-search", "splide__svg")} />
             </div>
           </SplideSlide>
           <SplideSlide>
-            <div className={style.partners__slpideElem}>
-              <RadioShack className={style.partners__svg} />
+            <div className={cl(style.partners__slpideElem, "slide-search")}>
+              <RadioShack className={cl("svg-search", "splide__svg")} />
             </div>
           </SplideSlide>
           <SplideSlide>
-            <div className={style.partners__slpideElem}>
-              <Tendo className={style.partners__svg} />
+            <div className={cl(style.partners__slpideElem, "slide-search")}>
+              <Tendo className={cl("svg-search", "splide__svg")} />
             </div>
           </SplideSlide>
         </Splide>
